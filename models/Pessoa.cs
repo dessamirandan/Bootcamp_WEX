cb335401cf4a36f948eaa6ead5a7c5af691776b5
Andressa Miranda <amirandamachado@gmail.com>
2025-06-03 19:23:16 -0300

    criando classes

---
+++ models/Pessoa.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Estrutura básica de uma classe
+namespace Bootcamp_WEX.models
+{
+    public class Pessoa //classe é uma representacao de objetos do mundo real.
+    {
+        public string Nome { get; set; } //Criando uma classe, sempre inicia com lera maiúscula
+        public int Idade { get; set; } //prop para criar um modelo
+        public void Apresentar(string @class) //utilizando palavras reservadas. NAO RECOMENDADO!
+        {
+            Console.WriteLine($"Olá, meu nome {Nome}, e possuo {Idade} anos."); //{Classe = Console e Método = .WriteLine()} e dentro está o parâmetro oua rgumento
+        }
+
+//ATRIBUTOS PRIEMIRO, DEPOIS MÉTODOS
+
+
+    }
+}
