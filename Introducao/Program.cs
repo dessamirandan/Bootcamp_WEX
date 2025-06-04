2acedbd68e34f30b8793a153586cd4b2cac3beff
Andressa Miranda <amirandamachado@gmail.com>
2025-06-03 21:02:53 -0300

    sintaxe e tipos de dados

--- Introducao/Program.cs
+++ Introducao/Program.cs
@@ -1,4 +1,28 @@
 ﻿
 //Reconhecendo as classes criadas
-using models;
+using Introducao.models;
 
+Pessoa pessoa1 = new Pessoa(); //variável pessoa1 do tipo Pessoa. Começa com minuscula camelCase
+pessoa1.Nome = "Andressa";
+pessoa1.Idade = 28;
+pessoa1.Apresentar(); //isso é um método. Começa com maiuscula Case
+
+string texto = "Olá mundo!";
+int inteiro = 10;
+double naturais = 1.2; //Padrão para operações matemáticas
+decimal valor = 1.80m; //Finanças, preços (alta precisão) sufixo -m
+float flutuante = 1.2f; //Medidas físicas (altura, distância) sufixo -f
+bool condicao = true;
+
+Console.WriteLine("Uma string: " + texto);
+Console.WriteLine("Um inteiro: " + inteiro);
+Console.WriteLine("Um natural: " + naturais);
+Console.WriteLine("Um decimal: " + valor);
+Console.WriteLine("Um float: " + flutuante);
+Console.WriteLine("Um booleano: " + condicao);
+
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine("Hoje é: " + dataAtual);
+
+Console.WriteLine("O mês é: " + dataAtual.ToString("MMMM")); //exibe mês por extenso
--- Introducao/models/Pessoa.cs
+++ Introducao/models/Pessoa.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 //Estrutura básica de uma classe
-namespace Bootcamp.models
+namespace Introducao.models //namespace
 {
     public class Pessoa //classe é uma representacao de objetos do mundo real.
     {
         public string Nome { get; set; } //Criando uma classe, sempre inicia com lera maiúscula
         public int Idade { get; set; } //prop para criar um modelo
-        public void Apresentar(string @class) //utilizando palavras reservadas. NAO RECOMENDADO!
+        public void Apresentar() //utilizando palavras reservadas. NAO RECOMENDADO! (string @class)
         {
-            Console.WriteLine($"Olá, meu nome {Nome}, e possuo {Idade} anos."); //{Classe = Console e Método = .WriteLine()} e dentro está o parâmetro oua rgumento
+            Console.WriteLine($"Olá, meu nome {Nome}, e possuo {Idade} anos.\n"); //{Classe = Console e Método = .WriteLine()} e dentro está o parâmetro oua rgumento
         }
 
-//ATRIBUTOS PRIEMIRO, DEPOIS MÉTODOS
+//ATRIBUTOS PRIMEIRO, DEPOIS MÉTODOS
 
 
     }
