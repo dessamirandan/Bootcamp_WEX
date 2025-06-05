3adb01af7ae8841d05731aa38eae22f024ec8987
Andressa Miranda <amirandamachado@gmail.com>
2025-06-05 20:02:43 -0300

    Operadores logicos

---
+++ Operadores/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//OR PIPE ||
+
+bool maiorDeIdade = true;
+bool possuiAutorizacaoDoResponsavel = false;
+
+if (maiorDeIdade || possuiAutorizacaoDoResponsavel)
+{
+  Console.WriteLine("Pode acessar a balada");
+}
+else
+{
+  Console.WriteLine("Você não pode entrar na balada!");
+}
+
+// AND &&
+
+bool presencaMinima = true;
+int possuiMedia = 7;
+
+if (presencaMinima && possuiMedia >= 7)
+{
+  Console.WriteLine("Você foi aprovado");
+}
+else
+{
+  Console.WriteLine("Você foi reprovado");
+}
