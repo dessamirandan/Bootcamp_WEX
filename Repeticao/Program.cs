140eb1056016f807dd5bc19358bf4a4ecec66b50
Andressa Miranda <amirandamachado@gmail.com>
2025-06-05 21:36:35 -0300

    Estrutura de repetição

---
+++ Repeticao/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int numero = 2;
+
+Console.WriteLine($"PARA:");
+for (int contador = 0; contador <= 10; contador++)
+{
+  Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
+
+Console.WriteLine($"ENQUANTO:");
+int conta = 0;
+
+while (conta <= 6)
+{
+  Console.WriteLine($"{numero} x {conta} = {numero + conta}");
+  conta++;
+
+}
+
+Console.WriteLine($"QUANDO:");
+
+int dado = 0, soma = 0;
+
+do
+{
+  Console.WriteLine("Digite um número: (0 para parar)");
+  dado = Convert.ToInt32(Console.ReadLine());
+
+  soma += dado;
+}
+while (dado != 0);
+
+Console.WriteLine($"O total da soma dos numeros digitados é: {soma}");
+
+
+Console.ReadLine();
+
