dc2ecf8dc74f192a5c263d4b1b741d61dce2d3c6
Andressa Miranda <amirandamachado@gmail.com>
2025-06-15 18:40:30 -0300

    Aula de Programacao orientada a objetos, criando classes, objetos e construtores

---
+++ POO/Models/ContaCorrente.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            Saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+        private decimal Saldo;
+
+        public void Sacar(decimal ValorDeSaque)
+        {
+            if (ValorDeSaque > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+            else
+            {
+                Saldo -= ValorDeSaque;
+                Console.WriteLine($"Saque de {ValorDeSaque}, realizado com sucesso. Seu saldo é de {Saldo}");
+            }
+        }
+        public void Depositar(decimal valorDeDeposito)
+        {
+            Saldo += valorDeDeposito;
+            Console.WriteLine($"Depósito de {valorDeDeposito}, realizado com sucesso. Seu saldo é de {Saldo}");
+
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Seu saldo é de {Saldo}");
+        }
+
+    }
+}
--- POO/Program.cs
+++ POO/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using POO.Models;
+      //Pessoa pessoa1 = new Pessoa();
+      //pessoa1.Nome = "Andressa";
+     // pessoa1.Idade = 28;
+
+      //pessoa1.Apresentar();
+
+      ContaCorrente c1 = new ContaCorrente(123, 1400);
+      c1.Depositar(600);
+c1.Sacar(100);
+c1.ExibirSaldo();
+
